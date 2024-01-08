faf7506038c5d9b71b78a8e504137360233b53cd
Gopal Kumar Das <krgopalkumar706@gmail.com>
2024-01-08 15:45:26 +0530

    chnge img src

--- W3SCHOOLS/app.js
+++ W3SCHOOLS/app.js
@@ -8,3 +8,17 @@
 // console.log(clickbtn);
 
 // Js Intro
+
+let imagetag = document.querySelector("#image")
+let changeBtn = document.querySelector("#chnagebtn")
+let a = 0;
+changeBtn.addEventListener("click",()=>{
+    if (a == 0) {
+        imagetag.src = "Assets/fl2.jpg";
+        a = 1;
+    } else {
+        imagetag.src = "Assets/flw1.jpg";
+        a = 0;
+    }
+    
+})
