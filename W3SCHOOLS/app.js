e837b85397e7021c253e69041136f2d3a09e23a5
Gopal Kumar Das <krgopalkumar706@gmail.com>
2024-01-08 15:15:23 +0530

    w3school practice added

---
+++ W3SCHOOLS/app.js
@@ -0,0 +1,10 @@
+// Js Home
+// let clickbtn = document.getElementById("clickme");
+// let span = document.getElementById('textspan');
+// clickbtn.addEventListener('click',()=>{
+//     let date = new Date();
+//     span.innerText = date;
+// });
+// console.log(clickbtn);
+
+// Js Intro
